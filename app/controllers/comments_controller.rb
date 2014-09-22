9843b8d74e390d012fdb06d3e8efd82ba3058256
grzzly <thomas.karin@me.com>
2014-09-23 01:40:23 +0200

    fixed multiple rendering issue.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
 		fullname = 't3_' + params[:thread_id].to_s
 		Comment.get_comments(fullname)
 
-		@comments = Comment.where("thread_id = ?", fullname)
+		@comments = Comment.where("thread_id = ? AND parent_id = ?", fullname, 0)
 		render @comments
 	end
 end
