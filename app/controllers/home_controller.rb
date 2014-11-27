ead273173fb32334e2111574a492e7a00ab434a6
grzzly <thomas.karin@me.com>
2014-11-27 22:47:43 +0100

    longer timeframe for showcasing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-		@match_threads = Matchthread.where(created_at: (Time.now - 12.hour)..Time.now).order('score DESC')
+		@match_threads = Matchthread.where(created_at: (Time.now - 48.hour)..Time.now).order('score DESC')
   end
 end
